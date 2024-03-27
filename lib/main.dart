34ac1f7571e95da0e453863588bf631295ea8561
Rehab Hamdy <80859649+Rehab-Hamdi@users.noreply.github.com>
2024-03-27 07:19:09 +0200

    main
    i add the routes of screens in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
-import 'package:al_huda/presentation/ui/home.dart';
-import 'package:al_huda/presentation/ui/spalsh_screen.dart';
+import 'package:al_huda/presentation/view/hadeth.dart';
+import 'package:al_huda/presentation/view/home.dart';
+import 'package:al_huda/presentation/view/payertimes.dart';
+import 'package:al_huda/presentation/view/quran.dart';
+import 'package:al_huda/presentation/view/sepha.dart';
+import 'package:al_huda/presentation/view/settings.dart';
+import 'package:al_huda/presentation/view/spalsh_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,7 +19,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Al_Huda',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
@@ -22,7 +27,12 @@ class MyApp extends StatelessWidget {
       initialRoute: SplashScreen.routeName,
       routes: {
         SplashScreen.routeName: (c) => const SplashScreen(),
-        HomeScreen.routeName: (c) => const HomeScreen(),
+        HomeScreen.routeName: (c) =>  const HomeScreen(),
+        SettingsScreen.routeName: (c) => const SettingsScreen(),
+        QuranScreen.routeName: (c) => const QuranScreen(),
+        SephaScreen.routeName: (c) => const SephaScreen(),
+        HadethScreen.routeName: (c) => const HadethScreen(),
+        PrayerTimesScreen.routeName: (c) => const PrayerTimesScreen(),
       },
       home: const SplashScreen(),
     );
